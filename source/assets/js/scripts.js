bea0f61596d65975d36450b73acaf0dd86f419a5
Artie Groove <apple2000@mail.ru>
2018-08-17 16:12:32 +0300

    Added some catalog pages

--- source/assets/js/scripts.js
+++ source/assets/js/scripts.js
@@ -9,4 +9,24 @@ $(document).ready( () => {
 		callbackForm.hide();
 		carousel.show();
 	});
+
+	let baseColor = $('.showcase').data('color');
+	let items, n;
+
+	// subcategory list colors
+	items = $('.showcase-subcategories .showcase-item');
+	n = items.length;
+	items.each( (i, item) => {
+		let itemColor = tinycolor(baseColor).spin(-10 * i);
+		$(item).css('background-color', itemColor.toString());
+	});
+
+	// product list colors
+	items = $('.showcase.showcase-products .showcase-item');
+	n = items.length;
+	items.each( (i, item) => {
+		let itemColor = tinycolor(baseColor).lighten(i * 100 / n);
+		$(item).css('background-color', itemColor.toString());
+	});
+
 });
